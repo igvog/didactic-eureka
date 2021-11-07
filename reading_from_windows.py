fc2fb4ea9d0785ef5f876bab68ec2ac57947016e
igvog <igvog88@gmail.com>
2021-11-07 21:25:23 +0600

    sftp read

---
+++ reading_from_windows.py
@@ -0,0 +1,20 @@
+import pysftp
+
+myHostname = "Ubuntu"
+myUsername = "samat"
+myPassword = "samat"
+
+with pysftp.Connection(host=myHostname, username=myUsername, password=myPassword) as sftp:
+    print("Connection succesfully stablished ... ")
+
+    # Switch to a remote directory
+    sftp.cwd('/home/samat/')
+
+    # Obtain structure of the remote directory '/var/www/vhosts'
+    directory_structure = sftp.listdir_attr()
+
+    # Print data
+    for attr in directory_structure:
+        print(attr.filename, attr)
+    
+# connection closed automatically at the end of the with-block
