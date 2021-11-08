f9cdd61bb11b99c12449311313fef15b931426e7
igvog <igvog88@gmail.com>
2021-11-08 12:27:51 +0600

    add test case

--- task_reading_pandas.py
+++ task_reading_pandas.py
@@ -1,22 +1,32 @@
 import pandas as pd
-import pysftp
+import csv
 
-myHostname = "yourserverdomainorip.com"
-myUsername = "root"
-myPassword = "12345"
+# test data file path, the fils is a csv file.
+test_data_file_path = '/home/samat/didactic-eureka/data/test_data.csv'
+# test data file object
+test_data_file_object = None
+# test data row list.
+test_data_row_list = list()
+# load test data from ./test_data.csv file.
+def load_test_data():
+    global test_data_file_object, test_data_row_list
+    # open test data csv file.
+    test_data_file_object = open(test_data_file_path, 'r')
+    # read the csv file and return the text line list.
+    csv_reader = csv.reader(test_data_file_object, delimiter=',')
+    for row in csv_reader:
+        test_data_row_list.append(row)
+    print('open and load data from test_data.csv complete.')
+# close and release the test data file object.
+def close_test_data_file():
+    global test_data_file_object
+    if test_data_file_object is not None:
+        test_data_file_object.close()
+        test_data_file_object = None
+        print('close file test_data.csv complete.')
 
-with pysftp.Connection(host=myHostname, username=myUsername, password=myPassword) as sftp:
-    print ("Connection succesfully stablished ... ")
-
-    # Define the file that you want to download from the remote directory
-    remoteFilePath = '/var/integraweb-db-backups/TUTORIAL.txt'
-
-    # Define the local path where the file will be saved
-    # or absolute "C:\Users\sdkca\Desktop\TUTORIAL.txt"
-    localFilePath = './TUTORIAL.txt'
-
-    sftp.get(remoteFilePath, localFilePath)
-
+load_test_data()
+close_test_data_file()
 # read file by using pandas
 df = pd.read_csv('/home/samat/didactic-eureka/data/people.csv')
 
