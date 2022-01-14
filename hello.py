3093dc2492684ae7b77c10fadba81a41d5ba2cf0
igvog <igvog88@gmail.com>
2022-01-14 20:42:07 +0500

    just test pylint

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
+#!/usr/bin/env python
+"""
+This script prompts a user to enter a message to encode or decode
+using a classic Caeser shift substitution (3 letter shift)
+"""
+
 print('Sagit Hello from Github actions')
